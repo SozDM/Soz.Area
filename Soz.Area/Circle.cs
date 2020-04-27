5e5103a56e4d5761deafe91352f37906f962e834
Dmitry <gamesmail57@gmail.com>
2020-04-27 16:52:49 +0300

    Библиотека для вычисления площади

---
+++ Soz.Area/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Soz.Area
+{
+    public class Circle
+    {
+        public int X { get; }
+        
+        public double GetArea()
+        {
+            double S = 3.14 * X;
+            return S;
+        }
+    }
+}
---
+++ Soz.Area/Triangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Soz.Area
+{
+    public class Triangle
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+
+        public Triangle(int x, int y, int z)
+        {
+            if ((x + y) <= z)
+            {
+                throw new ArgumentException($"Такой треугольник не существует. {x} + {y} <= {z}");
+            }
+
+            if ((x + z) <= y)
+            {
+                throw new ArgumentException($"Такой треугольник не существует. {x} + {z} <= {y}");
+            }
+
+            if ((z + y) <= x)
+            {
+                throw new ArgumentException( $"Такой треугольник не существует. {x} + {y} <= {z}");
+            }
+        }
+
+        public double GetArea()
+        {
+            double HalfPer = (X + Y + Z)/2; //HalfPerimeter 
+            double S = Math.Sqrt(HalfPer * (HalfPer - X) * (HalfPer - Y) * (HalfPer - Z));
+            return S;
+        }
+
+        public bool IsRectangular()
+        {
+            if ((X*X + Y*Y) == (Z * Z))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
